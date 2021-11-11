97281d85d018bbec68f44c10b7ad676e9ac1a6a4
Esther Carminatti <yunalesca002@gmail.com>
2021-11-11 15:10:24 +0000

    bookmark controller done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,38 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render 'new'
+    end
+  end
+
+  def find
+    @list = List.find(params[:id])
+  end
+
+  private
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+    redirect_to list_path(@list)
+  end
+
+  def restaurant_params
+    params.require(:list).permit(:name)
+  end
 end
