de902abacf22c8149103c22da8f1001808e28b64
Esther Carminatti <yunalesca002@gmail.com>
2021-11-11 14:46:25 +0000

    created lists controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  movie = Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::TvShows::Simpsons.quote,
+    poster_url: 'https://s.studiobinder.com/wp-content/uploads/2017/12/Movie-Poster-Template-Dark-with-Image.jpg?x81279',
+    rating: rand(0..10)
+  )
+  puts "Creating a new movie called #{movie.title}"
+end
